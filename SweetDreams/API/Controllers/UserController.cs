6b5c835fd8dccf1accf091d47ce41fbb54424962
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-11-03 01:18:55 +0200

    AppUser entity updated

---
+++ SweetDreams/API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UserController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+
+        return users;
+    }
+}
--- SweetDreams/API/Data/DataContext.cs
+++ SweetDreams/API/Data/DataContext.cs
@@ -6,7 +6,7 @@ namespace API.Data;
 public class DataContext : DbContext
 {
     public DbSet<AppUser> Users { get; set; }
-    
+
     public DataContext(DbContextOptions options) : base(options)
     {
     }
