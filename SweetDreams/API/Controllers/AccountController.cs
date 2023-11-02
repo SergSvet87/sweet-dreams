422da1d863139382e81bedd9a76c1d3b1d6a2738
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-11-03 01:55:55 +0200

    Register ready, but need to add validation.

---
+++ SweetDreams/API/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    public AccountController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    {
+        if (await UserExists(registerDto.Email))
+            return BadRequest("User with this email already exists.");
+        
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            UserName = registerDto.UserName,
+            Email = registerDto.Email.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return user;
+    }
+
+    private async Task<bool> UserExists(string email)
+    {
+        return await _context.Users.AnyAsync(x => x.Email == email.ToLower());
+    }
+}
--- SweetDreams/API/Controllers/UserController.cs
+++ SweetDreams/API/Controllers/UserController.cs
@@ -5,9 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UserController : ControllerBase
+public class UserController : BaseApiController
 {
     private readonly DataContext _context;
 
