e49ccad6d4ee33a95c9fe816ec7898a0e9306d7d
SergSvet87 <svetlichnij1905@gmail.com>
2023-11-02 12:51:00 +0200

    change getTheme

--- utils/getTheme.js
+++ utils/getTheme.js
@@ -4,14 +4,10 @@ export const getTheme = () => {
   const theme = `${global?.localStorage?.getItem('theme')}`;
 
   if (Object.values(themes).includes(theme)) return theme;
-  const matchMedia = '';
   if (typeof window !== 'undefined') {
-    const matchMedia = window.matchMedia('(prefers-color-scheme: light)');
-    return matchMedia;
+    const userMedia = window.matchMedia('(prefers-color-scheme: light)');
+    if (userMedia.matches) return themes.light;
   }
-  const userMedia = matchMedia;
-
-  if (userMedia.matches) return themes.light;
 
   return themes.dark;
 };
