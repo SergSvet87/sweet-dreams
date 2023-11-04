e4ad4e897276afbd9fa969cb48f1b88f63a2d28c
SergSvet87 <svetlichnij1905@gmail.com>
2023-11-04 14:06:46 +0200

    change admin page

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,11 +1,16 @@
+import { redirect } from 'next/navigation';
 import classNames from 'classnames';
 
 import styles from './admin.module.css';
 
 export default function AdminPage() {
-  return (
+  const auth = false;
+
+  return auth ? (
     <section className={styles.admin}>
       <div className={classNames('admin__container', styles.admin__wrapper)}>Admin</div>
     </section>
+  ) : (
+    redirect('/auth/admin')
   );
 }
