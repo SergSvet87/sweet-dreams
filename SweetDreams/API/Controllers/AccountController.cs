8ff3bf989119d8ecae04a14fa50d9e4de17dd6c6
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-11-07 17:58:14 +0200

    JWT token now returns after successful login or registration.

--- SweetDreams/API/Controllers/AccountController.cs
+++ SweetDreams/API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,19 +12,21 @@ namespace API.Controllers;
 public class AccountController : BaseApiController
 {
     private readonly DataContext _context;
+    private readonly ITokenService _tokenService;
 
-    public AccountController(DataContext context)
+    public AccountController(DataContext context, ITokenService tokenService)
     {
         _context = context;
+        _tokenService = tokenService;
     }
 
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Email))
             return BadRequest("User with this email already exists.");
 
-        if (!(await IsUserNameValid(registerDto.UserName)))
+        if (!IsUserNameValid(registerDto.UserName))
             return BadRequest("Username should contains first, second and middle names splited by space.");
         
         using var hmac = new HMACSHA512();
@@ -39,11 +42,16 @@ public class AccountController : BaseApiController
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
 
-        return user;
+        return new UserDto
+        {
+            UserName = user.UserName,
+            Email = user.Email,
+            Token = _tokenService.CreateToken(user)
+        };
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
         var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == loginDto.Email);
 
@@ -60,7 +68,12 @@ public class AccountController : BaseApiController
                 return Unauthorized("Invalid password.");
         }
 
-        return user;
+        return new UserDto
+        {
+            UserName = user.UserName,
+            Email = user.Email,
+            Token = _tokenService.CreateToken(user)
+        };
     }
 
     private async Task<bool> UserExists(string email)
@@ -68,7 +81,7 @@ public class AccountController : BaseApiController
         return await _context.Users.AnyAsync(x => x.Email == email.ToLower());
     }
 
-    private async Task<bool> IsUserNameValid(string userName)
+    private bool IsUserNameValid(string userName)
     {
         var splitedUserName = userName.Split(' ');
 
