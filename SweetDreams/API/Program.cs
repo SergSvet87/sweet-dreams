afa197d443d57d2ba3bce22fd2bc7a8afd96ed73
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-11-10 14:58:27 +0200

    Authorize button added to swagger. Swagger services moved to separate extension class.

--- SweetDreams/API/Program.cs
+++ SweetDreams/API/Program.cs
@@ -7,21 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Sweet Dreams API",
-        Description = "An ASP.NET Core Web API for Sweet Dreams online store.",
-    });
-    
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
-
+builder.Services.AddSwaggerServices(builder.Configuration);
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
