c242b480f94c3b11aa1a6acdd48755bbe5d65a83
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-12-26 12:09:33 +0200

    new

--- app/order/page.jsx
+++ app/order/page.jsx
@@ -8,12 +8,22 @@
 import styles from './order.module.css';
 
 import { OrderUserForm } from '../../components/order-user/OrderUserForm';
+import Image from 'next/image';
 // import { OrderGoods } from '../../components/order-goods/OrderGoods';
 
 export default function Order() {
   return (
     <section className={styles.order}>
       <div className={styles.container}>
+        <div className={styles.header_container}>
+          <Image
+            src="/images/order/checkout.svg"
+            alt="checkout arrow"
+            width={54}
+            height={26}
+          />
+          <h1 className={styles.header}>checkout</h1>
+        </div>
         <OrderUserForm />
         {/* <OrderGoods /> */}
       </div>
