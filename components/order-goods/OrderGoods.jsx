9eed5683674240ce5ca8e984fd51197170c066c7
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-12-27 12:50:05 +0200

    new

--- components/order-goods/OrderGoods.jsx
+++ components/order-goods/OrderGoods.jsx
@@ -11,7 +11,7 @@ export function OrderGoods() {
     <>
       <div className={styles.table}>
         <p className={styles.table_name}>Product</p>
-        <p className={styles.table_name}>Total</p>
+        {/* <p className={styles.table_name}>Total</p> */}
       </div>
       <ul>
         <div className={styles.good}>
@@ -23,24 +23,14 @@ export function OrderGoods() {
               <h2 className={styles.good_detail}>Item No: 112</h2>
             </div>
             <div className={styles.price_container}>
-              <p className={styles.price_single}>₴300</p>
-              <p className={styles.price_amount}>x 2</p>
-              <p className={styles.price_total}>₴600</p>
-            </div>
-          </div>
-        </div>
-        <div className={styles.good}>
-          <div className={styles.image}></div>
-          <div className={styles.info_container}>
-            <div>
-              <h2 className={styles.good_name}>Candy Bird</h2>
-              <h2 className={styles.good_detail}>Box of 10</h2>
-              <h2 className={styles.good_detail}>Item No: 112</h2>
-            </div>
-            <div className={styles.price_container}>
-              <p className={styles.price_single}>₴300</p>
-              <p className={styles.price_amount}>x 2</p>
-              <p className={styles.price_total}>₴600</p>
+              <div>
+                <p className={styles.price_single}>₴300</p>
+                <p className={styles.price_amount}>x 2</p>
+              </div>
+              <div>
+                <p className={styles.table_name}>Total</p>
+                <p className={styles.price_total}>₴600</p>
+              </div>
             </div>
           </div>
         </div>
