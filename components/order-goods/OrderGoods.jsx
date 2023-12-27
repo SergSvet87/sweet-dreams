f6e89890c6ecc684ce8b1af7e48f15df184fd911
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-12-27 16:49:52 +0200

    new

--- components/order-goods/OrderGoods.jsx
+++ components/order-goods/OrderGoods.jsx
@@ -10,27 +10,51 @@ export function OrderGoods() {
   return (
     <>
       <div className={styles.table}>
-        <p className={styles.table_name}>Product</p>
+        {/* <p className={styles.table_name}>Product</p> */}
         {/* <p className={styles.table_name}>Total</p> */}
       </div>
       <ul>
         <div className={styles.good}>
-          <div className={styles.image}></div>
-          <div className={styles.info_container}>
-            <div>
+          <div className={styles.info_container_image}>
+            <div className={styles.image}></div>
+            <div className={styles.info_container}>
               <h2 className={styles.good_name}>Candy Bird</h2>
-              <h2 className={styles.good_detail}>Box of 10</h2>
-              <h2 className={styles.good_detail}>Item No: 112</h2>
+              <div className={styles.good_detail}>
+                <p>Box of 10</p>
+                <p>Item No: 112</p>
+              </div>
             </div>
-            <div className={styles.price_container}>
-              <div>
-                <p className={styles.price_single}>₴300</p>
-                <p className={styles.price_amount}>x 2</p>
+          </div>
+          <div className={styles.price_container}>
+            <div className={styles.price_container_paragraph}>
+              <p className={styles.price_single}>₴300</p>
+              <p className={styles.table_name}>Total</p>
+            </div>
+            <div className={styles.price_container_paragraph}>
+              <p className={styles.price_amount}>x 2</p>
+              <p className={styles.price_total}>₴600</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.good}>
+          <div className={styles.info_container_image}>
+            <div className={styles.image}></div>
+            <div className={styles.info_container}>
+              <h2 className={styles.good_name}>Candy Bird</h2>
+              <div className={styles.good_detail}>
+                <p>Box of 10</p>
+                <p>Item No: 112</p>
               </div>
-              <div>
-                <p className={styles.table_name}>Total</p>
-                <p className={styles.price_total}>₴600</p>
-              </div>
+            </div>
+          </div>
+          <div className={styles.price_container}>
+            <div>
+              <p className={styles.price_single}>₴300</p>
+              <p className={styles.table_name}>Total</p>
+            </div>
+            <div>
+              <p className={styles.price_amount}>x 2</p>
+              <p className={styles.price_total}>₴600</p>
             </div>
           </div>
         </div>
