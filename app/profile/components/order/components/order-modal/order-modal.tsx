2e2409543f221481a7a530b426fcbefbcce071ce
Misha  Krasnonos <muzishutka7@gmail.com>
2023-12-28 01:03:38 +0200

    fix preview

--- app/profile/components/order/components/order-modal/order-modal.tsx
+++ app/profile/components/order/components/order-modal/order-modal.tsx
@@ -10,28 +10,28 @@ const OrderModal: FC<IOrderModal> = ({ order }) => {
   return (
     <div className={style.orderContainer}>
       <h1 className={style.orderNumber}>
-        Order № <span className={style.numberOrder}>{order.number}</span>
+        Order № <span className={style.numberOrder}>{order?.number}</span>
       </h1>
       <table>
         <tr className={style.orderParagraph}>
           <td>Order date:</td>
-          <td className={style.orderValues}>{order.date}</td>
+          <td className={style.orderValues}>{order?.date}</td>
         </tr>
         <tr className={style.orderParagraph}>
           <td className={style.contactName}>Delivery address:</td>
-          <td className={style.orderValues}>{order.address}</td>
+          <td className={style.orderValues}>{order?.address}</td>
         </tr>
         <tr className={style.orderParagraph}>
           <td className={style.contactName}>Recipient:</td>
-          <td className={style.orderValues}>{order.recipient}</td>
+          <td className={style.orderValues}>{order?.recipient}</td>
         </tr>
         <tr className={style.orderParagraph}>
           <td className={style.contactName}>Phone number:</td>
-          <td className={style.orderValues}>{order.phone}</td>
+          <td className={style.orderValues}>{order?.phone}</td>
         </tr>
       </table>
       <p className={style.orderTotal}>
-        Subtotal <span className={style.numberOrder}>{order.total}</span>
+        Subtotal <span className={style.numberOrder}>{order?.total}</span>
       </p>
     </div>
   );
