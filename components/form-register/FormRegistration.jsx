b6cbd975892cfcf84f457f61fa681bb5c2ad8124
Viacheslav Plevako <cheryec@ukr.net>
2024-02-28 12:51:38 +0200

    connect back-auth

--- components/form-register/FormRegistration.jsx
+++ components/form-register/FormRegistration.jsx
@@ -53,7 +53,6 @@ export default function FormRegistration({ register, errors }) {
   };
 
   const toggleAcceptWithRules = () => setAcceptWithRules(!acceptWithRules);
-  
 
   const handleFocusEmail = () => {
     labelTextEmail.current.style.opacity = '1';
@@ -61,7 +60,7 @@ export default function FormRegistration({ register, errors }) {
 
   const handleFocusPassword = () => {
     labelTextPassword.current.style.opacity = '1';
-  };  
+  };
 
   const handleFocusConfirm = () => {
     labelTextConfirm.current.style.opacity = '1';
@@ -229,9 +228,11 @@ export default function FormRegistration({ register, errors }) {
           name="remember"
         />
         <span className={styles.checkbox__span} />
-        <span className={styles.checkbox__text}>I understand and agree with <Link href='#'>Terms & Conditions</Link> and <Link href='#'>Privacy Policy</Link></span>
+        <span className={styles.checkbox__text}>
+          I understand and agree with <Link href="#">Terms & Conditions</Link> and{' '}
+          <Link href="#">Privacy Policy</Link>
+        </span>
       </label>
-
 
       <button className={styles.form__submit} type="submit">
         Create
@@ -247,7 +248,8 @@ export default function FormRegistration({ register, errors }) {
           height="24"
           viewBox="0 0 25 24"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg">
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             d="M22.3055 10.0415H21.5V10H12.5V14H18.1515C17.327 16.3285 15.1115 18 12.5 18C9.1865 18 6.5 15.3135 6.5 12C6.5 8.6865 9.1865 6 12.5 6C14.0295 6 15.421 6.577 16.4805 7.5195L19.309 4.691C17.523 3.0265 15.134 2 12.5 2C6.9775 2 2.5 6.4775 2.5 12C2.5 17.5225 6.9775 22 12.5 22C18.0225 22 22.5 17.5225 22.5 12C22.5 11.3295 22.431 10.675 22.3055 10.0415Z"
             fill="#FFC107"
@@ -268,10 +270,10 @@ export default function FormRegistration({ register, errors }) {
         Continue with google
       </button>
 
-      {/* <div className={styles.form__reg}>
-        If you have an account, please
+      <div className={styles.form__reg}>
+        {/* If you have an account, please */}
         <Link href="/auth/login"> Log in</Link>
-      </div> */}
+      </div>
     </>
   );
 }
