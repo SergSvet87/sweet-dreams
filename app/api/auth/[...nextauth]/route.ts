d9c0a3f25d92a97ed219316d21421f77caa8ba45
Viacheslav Plevako <cheryec@ukr.net>
2024-02-29 15:25:46 +0200

    change google register

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import type { NextAuthOptions } from 'next-auth'
+
+export const authOptions: NextAuthOptions = {
+  secret: 'xhbHxR5SnV+uvJaI5iGw3Zbcf8z0ztksXelsNd7EMY4=',
+  pages: {
+    signIn: '/auth/login',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: '581473169558-vo7fojm2c0f6bm2qdukm955e4v88r20c.apps.googleusercontent.com',
+      clientSecret: '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',
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
--- app/userboard/page.jsx
+++ app/userboard/page.jsx
@@ -1,11 +1,16 @@
 // import { instance } from '@/utils/client';
 
+import { authOptions } from '@app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
+
 export default async function UserBoard() {
+  const session = await getServerSession(authOptions);
+  console.log('session', session?.user);
   // const data = await instance.get('User');
   // console.log('data: ', data);
   return (
     <section className="userboard">
-      <div className="userboard__container">Hello User!</div>
+      <div className="userboard__container">Hello {session?.user.name}!</div>
     </section>
   );
 }
